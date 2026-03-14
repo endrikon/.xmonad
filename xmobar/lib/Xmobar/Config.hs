fa5fd4e27ccc1740693d97521a1c01b5345e4f87
Endrit Konjuhi <diti_97@windowslive.com>
2026-03-14 16:33:17 +0100

    make bar resize based on dpi settings

--- xmobar/lib/Xmobar/Config.hs
+++ xmobar/lib/Xmobar/Config.hs
@@ -1,26 +1,44 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ImportQualifiedPost #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 module Xmobar.Config (mkConfig) where
 
 import Xmobar
+import System.Process qualified as Process
+import Text.Read qualified as Read
+import Control.Exception qualified as Exception
+import Data.Text qualified as Text
+import Prelude
 
 mkConfig :: IO Config
-mkConfig =
+mkConfig = do
+    eDPI :: Either IOError String <- Exception.try $ Process.readCreateProcess (Process.shell "xrdb -query | grep dpi") ""
+    let mDPI :: Maybe Int = case eDPI of
+                Left _ -> Nothing
+                Right dpiNum -> Read.readMaybe
+                          $ Text.unpack
+                          $ Text.replace "\n" ""
+                          $ Text.replace "Xft.dpi:\t" ""
+                          $ Text.pack dpiNum
+        (fontSize, offset, heightPx) = case mDPI of
+                Just dpiNum -> if dpiNum >= 200 then ("20", 0, 26) else ("12", -1, 15)
+                Nothing -> ("12", -1, 15)
     pure $
         defaultConfig
-            { font = "DejaVu Sans Mono 12"
+            { font = "DejaVu Sans Mono " <> fontSize
             , additionalFonts =
                 [ "DejaVu Sans Mono italic 9"
-                , "Font Awesome 6 Free Solid 12"
+                , "Font Awesome 6 Free Solid " <> fontSize
                 ]
             , borderColor = "black"
             , border = FullB
             , bgColor = "black"
             , fgColor = "whitesmoke"
             , alpha = 128
-            , position = Top
-            , textOffset = -1
-            , iconOffset = -1
+            , position = TopSize C 100 heightPx
+            , textOffset = offset
+            , iconOffset = offset
             , lowerOnStart = True
             , pickBroadest = False
             , persistent = False
@@ -86,11 +104,11 @@ mkConfig =
             , sepChar = "%"
             , alignSep = "}{"
             , template =
-                "%UnsafeStdinReader%}"
+                " %UnsafeStdinReader%}"
                     ++ "{<box type=Bottom width=2 mb=1 color=#98be65><fc=#98be65>%alsa:default:Master%</fc></box>"
                     ++ " <box type=Bottom width=2 mb=1 color=#ecbe7b><fc=#ecbe7b><action=`alacritty -e htop`>%cpu%</action></fc></box>"
                     ++ " <box type=Bottom width=2 mb=1 color=#ff6c6b><fc=#ff6c6b><action=`alacritty -e htop`>%memory%</action></fc></box>"
                     ++ " <box type=Bottom width=2 mb=1 color=#a9a1e1><fc=#a9a1e1>%disku%</fc></box>"
                     ++ " <box type=Bottom width=2 mb=1 color=#da8548><fc=#da8548>%baticon%%battery%</fc></box>"
-                    ++ " <box type=Bottom width=2 mb=1 color=#46d9ff><fc=#46d9ff>%date%</fc></box>"
+                    ++ " <box type=Bottom width=2 mb=1 color=#46d9ff><fc=#46d9ff>%date%</fc></box> "
             }
